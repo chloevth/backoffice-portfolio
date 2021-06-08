3400403e77aa2942f54f257b3626e85cdefb88cc
acs <c.vauthier@codeur.online>
2021-06-08 16:46:40 +0200

    navbar index

--- index.php
+++ index.php
@@ -4,16 +4,35 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Portfolio créé par Chloé Vauthier.">
     <title>Chloé VAUTHIER | Graphiste & Web designer</title>
+    <link rel="icon" href="#"/>
     <link rel="stylesheet" href="main.css">
 </head>
 <body>
-    <button>
+    <header>
+        <nav>
+            <ul class="list__container">
+                <a href="index.php">
+                    <img src="assets/logo/logo-vecto.svg" alt="Logo du site Chloé Vauthier" class="logo">
+                </a>
+                <li class="list__item"><a href="#">PHOTO</a></li>
+                <li class="list__item"><a href="#">GRAPHISME</a></li>
+                <li class="list__item"><a href="#">WEB DESIGN</a></li>
+                <div class="cta__container">
+                <a href="register-form.php" class="cta__btn">S'inscrire</a>
+                <a href="login-form.php" class="cta__btn">Se connecter</a>
+                </div>
+            </ul>
+            
+        </nav>
+    </header>
+    <!-- <button>
         <a href="register-form.php">S'inscrire</a>
     </button>
     <button>
         <a href="login-form.php">Se connecter</a>
-    </button>
+    </button> -->
    
    
 </body>
