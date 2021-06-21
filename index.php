89b33cc049cbcf5b44ca8981073a88f83a8540cc
acs <c.vauthier@codeur.online>
2021-06-21 16:52:32 +0200

    maj index.php

--- index.php
+++ index.php
@@ -7,47 +7,30 @@
     <meta name="description" content="Portfolio créé par Chloé Vauthier.">
     <title>Chloé VAUTHIER &#124 Graphiste &#38 Web designer</title>
     <link rel="icon" href="#"/>
-    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="back-office.css">
 </head>
 <body>
     
 <!-- conteneur -->
-    <div class="container">
+    <div class="container__bo">
     <!-- header gauche -->
         
-        <div class="main__header">
-            <a href="#">BACK-OFFICE</a>
-            <p class="scroll">Scroll</p>
-            <a href="#">CHLOE VAUTHIER</a>
-            
-        </div>
+      
 
     <!-- fin header gauche -->
 
     <!-- bloc de cartes -->
 
-        <div class="bo__bloc--horizontal">
+       
+            <div class="main__header">
+                <a href="#"><h1>BACK-OFFICE</h1></a>
+            </div>
                        
-           
+           <div class="main__btn">
                 <a href="register-form.php" class="btn__style">S'inscrire</a>
-         
-           
                 <a href="login-form.php" class="btn__style">Se connecter</a>
-          
-           
-            <!-- <a href="#">
-                <div class="item un">
-                    <h2>TITRE</h2>
-                </div>
-            </a> -->
 
-  
-         
-
-        </div>
-
- 
- 
+           </div>
         
     </div>
 <!-- fin container -->
