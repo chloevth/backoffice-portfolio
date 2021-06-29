d4a04cf8a02a7c1fdb32a072705875976f0e7d5a
acs <c.vauthier@codeur.online>
2021-06-29 16:59:10 +0200

    pages home et add form

--- back/add-form.php
+++ back/add-form.php
@@ -3,51 +3,63 @@ session_start();
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ajouter un projet</title>
+    <meta name="description" content="Portfolio créé par Chloé Vauthier.">
+    <title>Chloé VAUTHIER &#124 Graphiste &#38 Web designer</title>
+    <link rel="icon" href="../assets/icones/favicon.ico"/>
+    <link rel="stylesheet" href="../assets/styles/main.css">
+    <link rel="stylesheet" href="../assets/styles/back.css">
 </head>
 <body>
-<form action="add-form-handler.php" method="post" enctype="multipart/form-data">
-        <div>
+
+
+<div class="container__bo">
+    <h1 class="add__form--title">Ajouter un projet</h1>
+    <form action="add-form-handler.php" method="post" enctype="multipart/form-data" class="add__form--form">
+        <div class="input__bo">
             <label for="input_title">Titre</label>
             <input type="text" id="input_title" name="project_title">
         </div>
-        <div>
+        <div class="input__bo">
             <label for="input_picture">Image</label>
             <input type="file" id="input_picture" name="project_picture">
         </div>
-        <div>
+        <div class="input__bo">
             <label for="input_begin">Date de démarrage</label>
             <input type="date" id="input_begin" name="project_begin">
         </div>
-        <div>
+        <div class="input__bo">
             <label for="input_end">Date de fin</label>
             <input type="date" id="input_end" name="project_end">
         </div>
-        <div>
+        <div class="input__bo">
             <label for="input_context">Contexte</label>
             <textarea name="project_context" id="input_context" cols="30" rows="10"></textarea>
         </div>
-        <div>
+        <div class="input__bo">
             <label for="input_specs">Spécifications fonctionnelles</label>
             <textarea name="project_specs" id="input_specs" cols="30" rows="10"></textarea>
         </div>
-        <div>
+        <div class="input__bo">
             <label for="input_githublink">Lien GitHub</label>
             <input type="text" id="input_githublink" name="project_githublink">
         </div>
-        <div>
+        <div class="input__bo">
             <label for="input_link">Lien du projet</label>
             <input type="text" id="input_link" name="project_link">
         </div>
-        <div>
+        <div class="input__bo">
             <input type="submit">
         </div>
 
     </form>
+
+
+</div>
+
 </body>
 </html>
--- back/home.php
+++ back/home.php
@@ -15,7 +15,76 @@ if ($_SESSION['username']) {
 
 ?>
 
+
 <!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Portfolio créé par Chloé Vauthier.">
+    <title>Chloé VAUTHIER &#124 Graphiste &#38 Web designer</title>
+    <link rel="icon" href="../assets/icones/favicon.ico"/>
+    <link rel="stylesheet" href="../assets/styles/main.css">
+    <link rel="stylesheet" href="../assets/styles/front.css">
+    <link rel="stylesheet" href="../assets/styles/back.css">
+</head>
+<body>
+    
+<!-- conteneur -->
+    <div class="container">
+    <!-- header gauche -->
+        <h1>PROJETS</h1>
+        <a href="add-form.php"><button class="btn__add">ajouter un projet</button></a>
+     
+
+    <!-- fin header gauche -->
+
+    <!-- bloc de cartes -->
+
+        <div class="bloc__horizontal">
+           
+        <?php
+            foreach ($result as $project) {
+        ?>
+
+            <a href="single.php?id=<?= $project['id'] ?>">
+                <div class="item" style="background:no-repeat center url('../assets/images/<?= $project['project_picture'] ?>');">
+                    <h2><?= $project['project_title'] ?></h2>
+                </div>
+            </a>
+            <?php
+                }
+            ?>        
+
+        </div>
+
+    <!-- fin bloc de cartes -->
+        
+        
+    </div>
+<!-- fin container -->
+
+<?php
+        foreach ($result as $project) {
+?>
+        <a href="project-details.php?id=<?= $project['id'] ?>"><?= $project['project_title'] ?></a>
+<?php
+        }
+?>
+
+
+
+  
+ 
+<script src="../assets/scripts/main.js"></script>
+<script src="../assets/scripts/modal.js"></script>
+
+</body>
+</html>
+
+
+<!-- <!DOCTYPE html>
 <html lang="en">
 
 <head>
@@ -28,18 +97,8 @@ if ($_SESSION['username']) {
 
 <body>
 
-    <?php
-        foreach ($result as $project) {
-    ?>
-        <a href="project-details.php?id=<?= $project['id'] ?>"><?= $project['project_title'] ?></a>
-    <?php
-        }
-    ?>
-
-
-
-    <a href="add-form.php"><button>ajouter un projet</button></a>
+   
 
 </body>
 
-</html>
+</html> -->
