381df60e658e93a2f50db86023c515ba0acac9da
acs <c.vauthier@codeur.online>
2021-11-09 16:00:12 +0100

    sauvegarde avant mise en production

--- back/add-form-handler.php
+++ back/add-form-handler.php
@@ -59,10 +59,7 @@ if ($_SESSION['username']) {
                     }
             
                     // Check if file already exists
-                    if (file_exists($target_file)) {
-                        echo "Sorry, file already exists.";
-                        $uploadOk = 0;
-                    }
+                
             
                     // Check file size
                     if ($_FILES["project_picture"]["size"] > 500000) {
